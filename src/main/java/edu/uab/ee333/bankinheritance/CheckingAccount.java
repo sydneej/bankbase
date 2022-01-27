d174140ad0a132c897c6ca6c8ed116a7705b216c
Sydnee Stewart <sydneej@uab.edu>
2022-01-26 21:03:42 -0600

    Refactored main into TestBank class

--- src/main/java/edu/uab/ee333/bankinheritance/CheckingAccount.java
+++ src/main/java/edu/uab/ee333/bankinheritance/CheckingAccount.java
@@ -7,77 +7,79 @@ package edu.uab.ee333.bankinheritance;
  */
 public class CheckingAccount extends BankAccount {
 
-  /**
-   * Construct a checking account given
-   *
-   * @param name owner
-   * @param balance in cents
-   */
-  public CheckingAccount(String name, int balance) {
-    super(name, balance);
-  }
-
-  /**
-   * toString - convert to a string representation showing the name of the checking account and the
-   * present balance
-   *
-   * @return a string representation of a checking account
-   */
-  @Override
-  public String toString() {
-    return name + "'s checking account has " + balance + " cents in it at present";
-  }
-
-  /**
-   * clearCheck - processes a check with overdraft penalty
-   *
-   * @param cents - amount of check
-   * @return boolean - true if the check clears
-   */
-  public boolean clearCheck(int cents) {
-    if (withdraw(cents)) {
-      return true;
-    } else {
-      balance -= 1500;
-      return false;
+    /**
+     * Construct a checking account given
+     *
+     * @param name owner
+     * @param balance in cents
+     */
+    public CheckingAccount(String name, int balance) {
+        super(name, balance);
     }
-  }
 
-  /** Assess a 150 cent fee each month */
-  @Override
-  public void assessMonthlyFee() {
-    balance -= 150;
-  }
+    /**
+     * toString - convert to a string representation showing the name of the
+     * checking account and the present balance
+     *
+     * @return a string representation of a checking account
+     */
+    @Override
+    public String toString() {
+        return name + "'s checking account has " + balance + " cents in it at present";
+    }
 
-  /**
-   * Small program to test this class
-   *
-   * @param args from command line - unused
-   */
-  public static void main(String[] args) {
+    /**
+     * clearCheck - processes a check with overdraft penalty
+     *
+     * @param cents - amount of check
+     * @return boolean - true if the check clears
+     */
+    public boolean clearCheck(int cents) {
+        if (withdraw(cents)) {
+            return true;
+        } else {
+            balance -= 1500;
+            return false;
+        }
+    }
 
-    CheckingAccount mine = new CheckingAccount("David Green", 200000);
-    System.out.println("The account has " + mine.getBalance());
-    System.out.println(mine);
+    /**
+     * Assess a 150 cent fee each month
+     */
+    @Override
+    public void assessMonthlyFee() {
+        balance -= 150;
+    }
 
-    mine.deposit(1000);
-    System.out.println(mine);
+    /**
+     * Small program to test this class
+     *
+     * @param args from command line - unused
+     */
+    public static void main(String[] args) {
 
-    mine.withdraw(200000);
-    System.out.println(mine);
+        CheckingAccount mine = new CheckingAccount("David Green", 200000);
+        System.out.println("The account has " + mine.getBalance());
+        System.out.println(mine);
 
-    // trying to remove too much
-    boolean status = mine.withdraw(200000);
-    System.out.println(mine);
-    System.out.println("Withdraw returned " + status);
+        mine.deposit(1000);
+        System.out.println(mine);
 
-    System.out.println(mine.clearCheck(1));
-    System.out.println(mine);
+        mine.withdraw(200000);
+        System.out.println(mine);
 
-    System.out.println(mine.clearCheck(10000));
-    System.out.println(mine);
+        // trying to remove too much
+        boolean status = mine.withdraw(200000);
+        System.out.println(mine);
+        System.out.println("Withdraw returned " + status);
 
-    mine.assessMonthlyFee();
-    System.out.println(mine);
-  }
+        System.out.println(mine.clearCheck(1));
+        System.out.println(mine);
+
+        System.out.println(mine.clearCheck(10000));
+        System.out.println(mine);
+
+        mine.assessMonthlyFee();
+        System.out.println(mine);
+    }
 }
